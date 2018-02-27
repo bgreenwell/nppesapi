9b4dfb25b6650950a745554c0ac9e2969948d34e
Dan Garmat <dgarmat@lhs.org>
2018-02-27 14:28:03 -0800

    attempting to make httr load when package is loaded so later code runs without loading httr separately

--- R/nppes_api.R
+++ R/nppes_api.R
@@ -1,36 +1,44 @@
 #' NPPES API
-#' 
+#'
 #' Query the NPPES NPI registry.
-#' 
+#'
 #' @param query Character string representing a valid query.
-#' 
-#' @return An bject of class \code{"nppes_api"}; essentially, an R list with 
+#'
+#' @return An bject of class \code{"nppes_api"}; essentially, an R list with
 #' various components depending on the submitted query.
 #'
 #' @rdname nppes_api
 #'
 #' @export
-#' 
-#' @examples 
+#'
+#' @examples
 #' nppes_api("city=dayton")
 #' nppes_api("number=1124021324")
+
+if(!"httr"%in%installed.packages()){
+  install.packages("httr")
+}
+
+suppressPackageStartupMessages(library("httr"))
+library("httr")
+
 nppes_api <- function(query) {
-  
+
   # Query the NPPES API
   url <- modify_url("https://npiregistry.cms.hhs.gov/api/", query = query)
   resp <- GET(url)
   if (http_type(resp) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-  
+
   # Parse the returned JSON file
   parsed <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = FALSE)
-  
+
   # Turn API errors into R errors
   if (http_error(resp)) {
     stop(
       sprintf(
-        "NPPES API request failed [%s]\n%s\n<%s>", 
+        "NPPES API request failed [%s]\n%s\n<%s>",
         status_code(resp),
         parsed$message,
         parsed$documentation_url
@@ -38,7 +46,7 @@ nppes_api <- function(query) {
       call. = FALSE
     )
   }
-  
+
   # Return a nppes_api object (a list)
   structure(
     list(
@@ -48,12 +56,12 @@ nppes_api <- function(query) {
     ),
     class = "nppes_api"
   )
-  
+
 }
 
 
 #' @rdname nppes_api
-#' 
+#'
 #' @export
 print.nppes_api <- function(x, ...) {
   cat("<NPPES ", x$path, ">\n", sep = "")
@@ -63,11 +71,11 @@ print.nppes_api <- function(x, ...) {
 
 
 #' NPPES Fields
-#' 
+#'
 #' Print available NPPES field values that can be queried.
-#' 
+#'
 #' @references A vector containing the names of the available for querying.
-#' 
+#'
 #' @export
 nppes_fields <- function() {
   c("number")
