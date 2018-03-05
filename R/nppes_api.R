2180ac9b819f4b838953b24ef0171e96e8df7548
Dan Garmat <dgarmat@lhs.org>
2018-03-05 03:05:00 -0800

    added npi_lookup function

--- R/nppes_api.R
+++ R/nppes_api.R
@@ -97,7 +97,7 @@ nppes_fields <- function() {
 
 npi_specialty <- function(npi) {
   if(nchar(npi) != 10){
-    stop("NPI to query for specialty must be of length 10")
+    stop(paste0("NPI to query for specialty must be of length 10. NPI ", npi, " is not."))
   }
   nppes_result <- nppes_api(paste0("number=", npi))
   specialty <- nppes_result$content$results[[1]]$taxonomies[[1]]$desc
@@ -105,3 +105,40 @@ npi_specialty <- function(npi) {
   data.frame("number" = npi, "specialty" = specialty, "last_updated" = last_updated)
 }
 
+
+#' NPI Lookup
+#'
+#' Query the NPPES NPI registry with multiple NPIs and return provider information.
+#'
+#' @param npis Vector of valid 10 digit NPIs (National Provider Identifier). Can be a numeric, character or mixed vector.
+#'
+#' @return An object of class \code{"data.frame"}; containing the NPIs queried, and the information requested.
+#' One per row. Currently only supports field = speciality.
+#'
+#' @rdname npi_lookup
+#'
+#' @export
+#'
+#' @examples
+#' npi_lookup(c(1649246182, 1831117050))
+
+npi_lookup <- function(npis, field = "specialty") {
+  # check if field being mapped is one there is a function for
+  acceptable_fields <- c("specialty") #, "address")  # will add address later!
+  if(!(field %in% acceptable_fields)){
+    stop(paste0(field, " field is not one of the acceptable fields. It should be one of these: ",
+                paste(acceptable_fields, collapse = ", ")))
+  }
+
+  # map according to field specified
+  if(field == "specialty"){
+    npilist <- lapply(npis, npi_specialty)
+  }
+
+  # current plan for this function is a list of ifs, but what if want more than one field?
+  # would overwrite it
+
+  # combine all results into a data frame
+  do.call(rbind, npilist)
+  # note this will error out it any npi returned an error
+}
